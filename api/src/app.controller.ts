70a35cb1df1cd6076df2d7359751cb9c7e3ec159
Pyae Sone Win <pyaesone.win@wavemoney.com.mm>
2024-04-03 05:16:48 +0630

    feat: 🎸 init project with auth login

---
+++ api/src/app.controller.ts
@@ -0,0 +1,26 @@
+import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { AuthService } from './auth/auth.service';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
+
+@Controller()
+export class AppController {
+  constructor(private authService: AuthService) {}
+
+  @Get()
+  getHello(): string {
+    return 'Hello World!';
+  }
+
+  @UseGuards(AuthGuard('local'))
+  @Post('auth/login')
+  async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
+  }
+}
